a22e0d67e950e0d578f52736585a3bc114d6fde0
MaximJam <maxim.sanse.jamar@gmail.com>
2026-03-31 15:00:55 +0200

    Updated console
    Signed-off-by: MaximJam <maxim.sanse.jamar@gmail.com>

--- src/SDK/SubsidiaryHydrator.php
+++ src/SDK/SubsidiaryHydrator.php
@@ -4,13 +4,32 @@ namespace BayWaReLusy\UsersAPI\SDK;
 
 use Laminas\Hydrator\ClassMethodsHydrator;
 
-/**
- * @method SubsidiaryEntity hydrate(array $data, object $object)
- */
-class SubsidiaryHydrator extends ClassMethodsHydrator
+class SubsidiaryHydrator
 {
+    private ClassMethodsHydrator $hydrator;
+
     public function __construct()
     {
-        parent::__construct(false);
+        $this->hydrator = new ClassMethodsHydrator(false);
+    }
+
+    /**
+     * @param array<mixed> $data
+     * @param object $object
+     * @return object
+     */
+    public function hydrate(array $data, object $object): object
+    {
+        return $this->hydrator->hydrate($data, $object);
+    }
+
+    /**
+     * @param object $object
+     * @return array<mixed>
+     */
+    public function extract(object $object): array
+    {
+        $data = $this->hydrator->extract($object);
+        return $data;
     }
 }
